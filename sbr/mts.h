a09996a5e5197f526ccd81607d2290bc42a1c205
Ralph Corderoy <ralph@inputplus.co.uk>
2017-11-16 23:08:45 +0000

    mts.h: Base header on implementation.
    Normally done as part of extracting the new header file from a larger
    one, but this file already existed so it had to be moved as one commit
    and altered in the next, otherwise git would break its history.

--- sbr/mts.h
+++ sbr/mts.h
@@ -1,11 +1,13 @@
-/* mts.h -- definitions for the mail system
- */
+/* mts.h -- definitions for the mail transport system
+ *
+ * This code is Copyright (c) 2017, by the authors of nmh.  See the
+ * COPYRIGHT file in the root directory of the nmh distribution for
+ * complete copyright information. */
 
 /*
- * Local and UUCP Host Name
+ * Separators
  */
-char *LocalName(int);
-char *SystemName(void);
+#define MMDF_DELIM "\001\001\001\001\n"
 
 /*
  * Mailboxes
@@ -14,35 +16,21 @@ extern char *mmdfldir;
 extern char *mmdflfil;
 extern char *uucpldir;
 extern char *uucplfil;
+
+#define	MAILDIR	(mmdfldir && *mmdfldir ? mmdfldir : getenv("HOME"))
+#define	MAILFIL	(mmdflfil && *mmdflfil ? mmdflfil : getusername(1))
+
 extern char *spoollocking;
-
-#define	MAILDIR	(mmdfldir && *mmdfldir ? mmdfldir : getenv ("HOME"))
-#define	MAILFIL	(mmdflfil && *mmdflfil ? mmdflfil : getusername (1))
-
-char *getusername(int);
-char *getfullname(void);
-char *getlocalmbox(void);
-
-/*
- * Separators
- */
-#define MMDF_DELIM "\001\001\001\001\n"
-
-/*
- * Read mts.conf file
- */
-void mts_init (void);
 
 /*
  * MTS specific variables
  */
-
 /* whether to speak SMTP, and over the network or directly to sendmail */
 #define MTS_SMTP          0
 #define MTS_SENDMAIL_SMTP 1
 #define MTS_SENDMAIL_PIPE 2
 extern int sm_mts;
-void save_mts_method (const char *);
+void save_mts_method(const char *);
 
 extern char *sendmail;
 
@@ -54,6 +42,21 @@ extern char *servers;
 extern char *pophost;
 
 /*
- * Global MailDelivery File
+ * Global MailDelivery file
  */
 extern char *maildelivery;
+
+/*
+ * Read mts.conf file
+ */
+void mts_init(void);
+
+/*
+ * Local and UUCP Host Name
+ */
+char *LocalName(int);
+char *SystemName(void);
+
+char *getusername(int);
+char *getfullname(void);
+char *getlocalmbox(void);
