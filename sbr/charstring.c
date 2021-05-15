15ffd5c22023e80a53a6e875edff6844c0355fef
David Levine <levinedl@acm.org>
2021-05-15 08:34:53 -0400

    Fixed comment to say trailing NULL instead of newline.

--- sbr/charstring.c
+++ sbr/charstring.c
@@ -125,7 +125,7 @@ charstring_append_cstring (charstring_t dest, const char src[])
 
     if (num > 0) {
         charstring_reserve (dest, dest->cur - dest->buffer + num);
-        memcpy (dest->cur, src, num);  /* Exclude src's trailing newline. */
+        memcpy (dest->cur, src, num);  /* Exclude src's trailing NULL. */
         dest->cur += num;
         dest->chars += num;
     }
